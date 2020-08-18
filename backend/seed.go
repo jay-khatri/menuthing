b4bd35c77999077ac8c1e8b4a243411b2f74a400
Jay Khatri <jaykhatri@Jays-MacBook-Pro.local>
2020-08-18 12:30:39 -0400

    change

--- backend/seed.go
+++ backend/seed.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"strconv"
 
-func seed() {
-	fmt.Println("hi")
+	"github.com/jay-khatri/menuthing/backend/graph"
+	"github.com/jay-khatri/menuthing/backend/graph/model"
+)
+
+func seed(resolver *graph.Resolver) {
+	log.Println("seeding database")
+	ctx := context.Background()
+	// Create a Menu
+	menu, err := resolver.Mutation().CreateMenu(ctx, "Saigon", "Saigon Restaurant")
+	if err != nil {
+		log.Fatalf("error creating menu: %v", err)
+	}
+	// Create Categories
+	categories := []string{"Fish", "Chicken", "Appetizers"}
+	categoryIDs := []model.ObjectID{}
+	for i, c := range categories {
+		category, err := resolver.Mutation().CreateMenuCategory(ctx, c, menu.ID)
+		if err != nil {
+			log.Fatalf("error creating category: %v", err)
+		}
+		for _, item := range []int{1, 2, 3, 4, 5, 6, 7, 8} {
+			_, err := resolver.Mutation().CreateMenuItem(ctx, categories[i]+" "+strconv.Itoa(item), "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation", 1.23, menu.ID, category.ID)
+			if err != nil {
+				log.Fatalf("error creating menu item: %v", err)
+			}
+		}
+		categoryIDs = append(categoryIDs, category.ID)
+	}
+	// Create some menu items for each category.
+	log.Println("done seeding database")
 }
--- backend/server.go
+++ backend/server.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"context"
 	"flag"
 	"log"
 	"net/http"
 	"os"
-	"strconv"
 
 	"github.com/gorilla/handlers"
 	"github.com/rs/cors"
@@ -16,7 +14,6 @@ import (
 	"github.com/jay-khatri/menuthing/backend/database"
 	"github.com/jay-khatri/menuthing/backend/graph"
 	"github.com/jay-khatri/menuthing/backend/graph/generated"
-	"github.com/jay-khatri/menuthing/backend/graph/model"
 	"github.com/jay-khatri/menuthing/backend/sessions"
 )
 
@@ -32,31 +29,7 @@ func main() {
 	}
 
 	if *seedFlag {
-		log.Println("seeding database")
-		ctx := context.Background()
-		// Create a Menu
-		menu, err := resolver.Mutation().CreateMenu(ctx, "Saigon", "Saigon Restaurant")
-		if err != nil {
-			log.Fatalf("error creating menu: %v", err)
-		}
-		// Create Categories
-		categories := []string{"Fish", "Chicken", "Appetizers"}
-		categoryIDs := []model.ObjectID{}
-		for i, c := range categories {
-			category, err := resolver.Mutation().CreateMenuCategory(ctx, c, menu.ID)
-			if err != nil {
-				log.Fatalf("error creating category: %v", err)
-			}
-			for _, item := range []int{1, 2, 3, 4, 5, 6, 7, 8} {
-				_, err := resolver.Mutation().CreateMenuItem(ctx, categories[i]+" "+strconv.Itoa(item), "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation", 1.23, menu.ID, category.ID)
-				if err != nil {
-					log.Fatalf("error creating menu item: %v", err)
-				}
-			}
-			categoryIDs = append(categoryIDs, category.ID)
-		}
-		// Create some menu items for each category.
-		log.Println("done seeding database")
+		seed(resolver)
 		return
 	}
 
